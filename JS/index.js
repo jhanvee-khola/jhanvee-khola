669f9319a3e091e85f57c98bfab06194aea3c57d
Jhanvee Khola <77978729+jhanvee-khola@users.noreply.github.com>
2021-12-06 01:14:51 +0530

    Update index.js

--- JS/index.js
+++ JS/index.js
@@ -1,6 +1,6 @@
 const toTop = document.querySelector(".to-top");
 window.addEventListener("scroll", () => {
-  if (window.pageYOffset > 100) {
+  if (window.pageYOffset > 120) {
     toTop.classList.add("active");
   } else {
     toTop.classList.remove("active");

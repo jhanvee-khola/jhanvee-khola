eb0b146454c12d541ad00b4f0b8a228e6a48f125
Jhanvee Khola <77978729+jhanvee-khola@users.noreply.github.com>
2022-05-28 06:46:34 +0530

    Update index.js

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,4 @@
+// BACK-TO-TOP BUTTON
 const toTop = document.querySelector(".to-top");
 window.addEventListener("scroll", () => {
   if (window.pageYOffset > 120) {
@@ -6,3 +7,40 @@ window.addEventListener("scroll", () => {
     toTop.classList.remove("active");
   }
 })
+
+// THEME
+let theme = localStorage.getItem('theme')
+if(theme == null){
+	setTheme('light')
+}
+else{
+	setTheme(theme)
+}
+
+let themeDots = document.getElementsByClassName('theme-dot')
+for (var i=0; themeDots.length > i; i++){
+	themeDots[i].addEventListener('click', function(){
+		let mode = this.dataset.mode
+		console.log('Option clicked:', mode)
+		setTheme(mode)
+	})
+}
+
+function setTheme(mode){
+	if(mode == 'light'){
+		document.getElementById('theme-style').href = 'Styles/light.css'
+	}
+	if(mode == 'dark'){
+		document.getElementById('theme-style').href = 'Styles/dark.css'
+	}
+	if(mode == 'pink'){
+		document.getElementById('theme-style').href = 'Styles/pink.css'
+	}
+  if(mode == 'lemon'){
+		document.getElementById('theme-style').href = 'Styles/lemon.css'
+	}
+	if(mode == 'blue'){
+		document.getElementById('theme-style').href = 'Styles/blue.css'
+	}
+	localStorage.setItem('theme', mode)
+}
